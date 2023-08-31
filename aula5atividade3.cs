07176ba20bc2bac88a10009a5f68a4aee728a49e
PabloClopes123 <143664403+PabloClopes123@users.noreply.github.com>
2023-08-30 21:21:58 -0300

    Update aula5atividade3.cs

--- aula5atividade3.cs
+++ aula5atividade3.cs
@@ -12,19 +12,18 @@ namespace InformacoesUsuario
             Console.Write("Digite sua idade: ");
             string idadeStr = Console.ReadLine();
 
-            // Exibir o nome completo em maiúsculo e minúsculo
+   
             Console.WriteLine($"Nome completo em maiusculo: {nomeCompleto.ToUpper()}");
             Console.WriteLine($"Nome completo em minusculo: {nomeCompleto.ToLower()}");
 
-            // Contar o número de caracteres no nome completo (excluindo espaços em branco)
+           
             int numCaracteres = nomeCompleto.Replace(" ", "").Length;
             Console.WriteLine($"Numero de caracteres no nome completo: {numCaracteres}");
 
-            // Converter a idade para um número inteiro com tratamento de erro
             int idade;
             if (int.TryParse(idadeStr, out idade))
             {
-                // Calcular o ano de nascimento e o ano em que o usuário fará 100 anos
+             
                 int anoAtual = DateTime.Now.Year;
                 int anoNascimento = anoAtual - idade;
                 int anoCemAnos = anoNascimento + 100;
